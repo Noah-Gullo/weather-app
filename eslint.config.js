51b46de854b6c2f942fb895c272690e6842937a7
Noah Gullo <noahgulloingram@gmail.com>
2026-05-06 16:33:43 -0700

    feat: setup basic file structure

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,5 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
-  },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
