013e1ee0410c510ae06ae21ea49f3d3e4c72be12
Noah Gullo <noahgulloingram@gmail.com>
2026-05-07 19:12:50 -0700

    feat: can get  data from a location from now to a week later

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import "./styles.css";
+import {format} from "date-fns"
 
-console.log("Hello, world");
+async function getData(location){
+    try{
+        const oneWeekInSeconds = 31536000;
+        const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" 
+        + location + "/" 
+        + format(Date.now(), "yyyy-M-d") + "/"
+        + format(Date.now() + oneWeekInSeconds, "yyyy-M-d")
+        + "?key=HX6ZYM4HCT33AWBA8V2HENJ28"; 
+        const response = await fetch(url);
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+getData("london");
