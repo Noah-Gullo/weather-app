5e6737f4b39c2f7b23b159f5a7c8438658a68f5e
Noah Gullo <noahgulloingram@gmail.com>
2026-05-07 21:05:25 -0700

    feat: add more properties to WeatherData

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
+import { da } from "date-fns/locale";
 import "./styles.css";
 import {format} from "date-fns"
+
+class WeatherData{
+    constructor(temp, tempmax, tempmin, icon, description){
+        this.temp = temp;
+        this.tempmax = tempmax;
+        this.tempmin = tempmin;
+        this.icon = icon;
+        this.description = description;
+    }
+
+    get temp(){
+        return this.temp;
+    }
+
+    get tempmax(){
+        return this.tempmax;
+    }
+
+    get tempmin(){
+        return this.tempmin;
+    }
+
+    get icon(){
+        return this.icon;
+    }
+
+    get description(){
+        return this.description;
+    }
+}
 
 async function getData(location){
     try{
@@ -8,13 +39,26 @@ async function getData(location){
         + location + "/" 
         + format(Date.now(), "yyyy-M-d") + "/"
         + format(Date.now() + oneWeekInSeconds, "yyyy-M-d")
-        + "?key=HX6ZYM4HCT33AWBA8V2HENJ28"; 
+        + "?key=" + "46NG7ZU8PRUUL8JJ8Q7KB5NB3"; 
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        processData(data);
     }catch(error){
         console.log(error);
     }
+}
+
+function processData(data){
+    console.log(data);
+    let filtered = new WeatherData(
+        temp = data.currentConditions.temp,
+        tempmax = data.currentConditions.tempmax,
+        tempmin = data.currentConditions.tempmin,
+        icon = data.currentConditions.icon,
+        description = data.currentConditions.description,
+    );
+    console.log("Temp: " + filtered.temp);
+    return filtered;
 }
 
 getData("london");
