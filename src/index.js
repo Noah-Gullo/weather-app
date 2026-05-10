7b4d8829ce25c695ebedb1620cbffec815664052
Noah Gullo <noahgulloingram@gmail.com>
2026-05-09 22:15:21 -0700

    fix: change to object literal and correctly gets data

--- src/index.js
+++ src/index.js
@@ -1,48 +1,15 @@
-import { da } from "date-fns/locale";
 import "./styles.css";
 import {format} from "date-fns"
-
-class WeatherData{
-    constructor(temp, tempmax, tempmin, icon, description){
-        this.temp = temp;
-        this.tempmax = tempmax;
-        this.tempmin = tempmin;
-        this.icon = icon;
-        this.description = description;
-    }
-
-    get temp(){
-        return this.temp;
-    }
-
-    get tempmax(){
-        return this.tempmax;
-    }
-
-    get tempmin(){
-        return this.tempmin;
-    }
-
-    get icon(){
-        return this.icon;
-    }
-
-    get description(){
-        return this.description;
-    }
-}
 
 async function getData(location){
     try{
         const oneWeekInSeconds = 31536000;
         const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" 
         + location + "/" 
-        + format(Date.now(), "yyyy-M-d") + "/"
-        + format(Date.now() + oneWeekInSeconds, "yyyy-M-d")
         + "?key=" + "46NG7ZU8PRUUL8JJ8Q7KB5NB3"; 
         const response = await fetch(url);
         const data = await response.json();
-        processData(data);
+        return processData(data);
     }catch(error){
         console.log(error);
     }
@@ -50,15 +17,20 @@ async function getData(location){
 
 function processData(data){
     console.log(data);
-    let filtered = new WeatherData(
-        temp = data.currentConditions.temp,
-        tempmax = data.currentConditions.tempmax,
-        tempmin = data.currentConditions.tempmin,
-        icon = data.currentConditions.icon,
-        description = data.currentConditions.description,
-    );
-    console.log("Temp: " + filtered.temp);
+    const filtered = {
+        "temp": data.currentConditions.temp,
+        "tempmax": data.days[0].tempmax,
+        "tempmin": data.days[0].tempmin,
+        "icon": data.currentConditions.icon,
+        "description": data.description,
+    };
     return filtered;
 }
 
-getData("london");
+let weatherData = await getData("morocco");
+console.log(weatherData);
+console.log("Temperature: " + weatherData.temp);
+console.log("Max Temperature: " + weatherData.tempmax);
+console.log("Minimum Temperature: " + weatherData.tempmin);
+console.log("Icon: " + weatherData.icon);
+console.log("Description: " + weatherData.description);
